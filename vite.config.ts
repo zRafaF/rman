51d80c79b381c5feb8c6134dc4685f3b3bdc436e
Caracol <rafaelfariasm@live.com>
2024-11-28 20:45:41 -0300

    removed build chunking

--- vite.config.ts
+++ vite.config.ts
@@ -14,14 +14,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react"],
-          "react-dom": ["react-dom"],
-        },
-      },
-    },
-  },
 });

429661768a4524b37fb54caef5f3d345e36b5a23
Caracol <rafaelfariasm@live.com>
2024-11-28 20:48:32 -0300

    readded build chunking and fixed homepage setup

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react"],
+          "react-dom": ["react-dom"],
+        },
+      },
+    },
+  },
 });

4f7482cfce12b6b237cec17f790266328426fc4a
Caracol <rafaelfariasm@live.com>
2024-11-28 20:54:24 -0300

    added base path to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://zrafaf.github.io/rman/",
+
   plugins: [
     react(),
     svgr(), // Add the SVGR plugin

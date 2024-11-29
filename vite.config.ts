63213d6e70b7cb229f59756ec14cb207dc4c0709
Caracol <rafaelfariasm@live.com>
2024-11-28 21:05:21 -0300

    removed pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import path from "path";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [
     react(),
     svgr(), // Add the SVGR plugin
-    VitePWA({ registerType: "autoUpdate" }),
+    // VitePWA({ registerType: "autoUpdate" }),
   ],
   resolve: {
     alias: {
